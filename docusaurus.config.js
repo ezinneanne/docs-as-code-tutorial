ce33c7e4a5f79d2bb73d40ed34278cad2f42a089
Ezinne Anne Emilia <onwukaezinneanne@gmail.com>
2025-01-26 20:38:38 +0100

    first commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,15 +15,15 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://ezinneanne.github.io/',
+  url: 'https://docs-as-code-tutorial',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs-as-code-tutorial/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'ezinneanne', // Usually your GitHub org/user name.
-  projectName: 'docs-as-code-tutorial', // Usually your repo name.
+  //organizationName: 'ezinneanne', // Usually your GitHub org/user name.
+  //projectName: 'docs-as-code-tutorial', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
